06b7a0bd482c15e5d338303649f7dd2f66d5624a
justincolangelo <justincolangelo@gmail.com>
2018-08-22 23:06:44 -0400

    try view again

--- src/android/Html5Video.java
+++ src/android/Html5Video.java
@@ -16,6 +16,7 @@ import android.content.res.AssetFileDescriptor;
 import android.content.res.Resources.NotFoundException;
 import android.os.Build;
 import android.util.Log;
+import android.View.View;
 
 public class Html5Video extends CordovaPlugin {
 	private static final String TAG = "Html5VideoCordovaPlugin";
@@ -74,7 +75,8 @@ public class Html5Video extends CordovaPlugin {
 			if (videoId != null) {
 				cordova.getActivity().runOnUiThread(new Runnable() {
 				     public void run() {
-				           webView.loadUrl("javascript:window.plugins.html5Video._play(" + videoId + ")");
+				     		webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+				     		webView.loadUrl("javascript:window.plugins.html5Video._play(" + videoId + ")");
 				     }
 				});
 				status = true;
